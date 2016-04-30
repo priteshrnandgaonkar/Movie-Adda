c2583ba2d90321b0fbd4dc8be8dde8b17e11ee0b
prit91 <pncooldude91@gmail.com>
2016-04-30 13:02:44 +0530

    Initial Commit

---
+++ Movie Adda/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Movie Adda
+//
+//  Created by Pritesh Nandgaonkar on 30/04/16.
+//  Copyright © 2016 Pritesh Nandgaonkar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
